01d2720f4b10d09c6941736aa27f1eba59b2f608
adrianfathurs <fatursetiawan80@gmail.com>
2026-03-11 15:31:06 +0700

    fix(cici): fixing bug current price

--- chatbot_xauusd_twelve_cici.js
+++ chatbot_xauusd_twelve_cici.js
@@ -118,7 +118,7 @@ async function checkSignal() {
     lastSignalTime = last.time;
     sendTelegram(
   `🚨 <b>PANTAU CICI GUYS</b> 🚨
-    Current Price : ${close.toFixed(2)}
+    Current Price : ${last.close.toFixed(2)}
     Current CCI Value : ${CCI_VALUE.toFixed(2)}`);
   }
 }
